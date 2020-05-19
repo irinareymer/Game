202c914aa6e462be8f27574a930f99a51f9adf01
Ирина Реймер <reymer.irina4@gmail.com>
2020-05-19 15:21:18 +0300

    roll to set parameters

--- core/src/Logic/GameLogic.java
+++ core/src/Logic/GameLogic.java
@@ -12,68 +12,61 @@ public class GameLogic {
     boolean isSetPower = false;
     boolean isSetSpeed = false;
     int rolled;
+    boolean isStarted = false;
 
     public GameLogic(EntitiesManager em) {
         this.em = em;
     }
 
     public void init() {
-
     }
 
     public void update(float dt) {
-        play();
+        while(!isStarted)
+            play();
 }
 
     public void play() {
-
-        //player1
         em.setCurrentPlayer(em.getPlayState().getPlayer1());
-        em.getCurrentPayer().setName("OLEG"); //todo
-        //System.out.println("Current" + em.getCurrentPayer().getName());
 
-        //setParameters for player1 //todo show param
-        boolean act = Gdx.input.isKeyJustPressed(Input.Keys.ENTER);
-        if (!isSetLuck && act){
-            rolled = em.getPlayState().getDice().rollDice();
-            em.getCurrentPayer().setLuck(rolled);
-            System.out.println("luck" + em.getPlayState().getDice().getRolled());
-            System.out.println(em.getCurrentPayer().getLuck());
-            isSetLuck = true;
-            act = false;
-        }
-        if (!isSetPower && isSetLuck && act){
-            rolled = em.getPlayState().getDice().rollDice();
-            em.getCurrentPayer().setPower(rolled);
-            System.out.println("power" + em.getPlayState().getDice().getRolled());
-            System.out.println(em.getCurrentPayer().getPower());
-            isSetPower = true;
-            act = false;
-        }
-        if (!isSetSpeed && isSetPower && isSetLuck && act){
-            rolled = em.getPlayState().getDice().rollDice();
-            em.getCurrentPayer().setSpeed(rolled);
-            System.out.println("speed" + em.getPlayState().getDice().getRolled());
-            System.out.println(em.getCurrentPayer().getSpeed());
-            isSetSpeed = true;
-        }
+        for (int i = 0; i < 2; i++) {
+            em.getCurrentPayer().setName("OLEG"); //todo
+            //System.out.println("Current" + em.getCurrentPayer().getName());
 
-
-        /*if (em.getPlayState().getKm().isAction() && isSetLuck && !isSetPower) {
-            em.getCurrentPayer().setPower(rollDice());
-            System.out.println(em.getCurrentPayer().getPower());
-            isSetPower = true;
+            //setParameters todo show param
+            boolean act = Gdx.input.isKeyJustPressed(Input.Keys.ENTER);
+            if (!isSetLuck && act) {
+                rolled = em.getPlayState().getDice().rollDice();
+                em.getCurrentPayer().setLuck(rolled);
+                System.out.println("luck" + em.getPlayState().getDice().getRolled());
+                System.out.println(em.getCurrentPayer().getLuck());
+                isSetLuck = true;
+                act = false;
+            }
+            if (!isSetPower && isSetLuck && act) {
+                rolled = em.getPlayState().getDice().rollDice();
+                em.getCurrentPayer().setPower(rolled);
+                System.out.println("power" + em.getPlayState().getDice().getRolled());
+                System.out.println(em.getCurrentPayer().getPower());
+                isSetPower = true;
+                act = false;
+            }
+            if (!isSetSpeed && isSetPower && isSetLuck && act) {
+                rolled = em.getPlayState().getDice().rollDice();
+                em.getCurrentPayer().setSpeed(rolled);
+                System.out.println("speed" + em.getPlayState().getDice().getRolled());
+                System.out.println(em.getCurrentPayer().getSpeed());
+                isSetSpeed = true;
+                act = false;
+            }
+            isSetSpeed = false;
+            isSetLuck = false;
+            isSetPower = false;
+            em.changeCurrentPlayer(em.getCurrentPayer());
         }
-        em.getCurrentPayer().setSpeed(rollDice());
-            System.out.println(em.getCurrentPayer().getSpeed());
+        isStarted = true;
+/*
 
-        //player2
-        em.setCurrentPlayer(em.getPlayState().getPlayer2());
-        em.getCurrentPayer().setName("MARY"); //todo
-        //setParameters for player2 //todo show param
-        em.getCurrentPayer().setLuck(rollDice());
-        em.getCurrentPayer().setPower(rollDice());
-        em.getCurrentPayer().setSpeed(rollDice());
 
         //loop
         while (em.getPlayState().getPlayer1().getCountItems() != 3 || em.getPlayState().getPlayer2().getCountItems() != 3) {
